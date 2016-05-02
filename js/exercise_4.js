85b7232f5a47aeb157b4732f4c35e979abf85f5b
plemoda <david.plemons@piedmontng.com>
2016-05-02 15:46:12 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -39,3 +39,27 @@ featureLayer.on('ready', function(){
       layer.bindPopup('Welcome to ' + layer.feature.properties.name);
     })
 })
+
+var clickHandler = function(e){
+ 	$('#info').empty();
+  
+  	var feature = e.target.feature;
+  
+  	$('#sidebar').fadeIn(400, functiion()){
+      var info = '';
+      
+      info += '<div>';
+      info += '<h2>' + feature.properties.name + '</h2>'
+      if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '</p>';
+      if (feature.properties.phone)  info += '<p>' + feature.properties.phone + '</p>';
+      if (feature.properties.website)  info += '<p><a href="' + feature.properties.website + '</a></p>';
+      info += '</div>';
+      $('#info') += '</div>';
+    })
+}
+
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    layer.on('click',clickHandller);
+  })
+})
