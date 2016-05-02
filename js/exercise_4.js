88b03a2b958546815100374eeea6fc1313dd7b4a
plemoda <david.plemons@piedmontng.com>
2016-05-02 16:13:02 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -41,30 +41,32 @@ featureLayer.on('ready', function(){
 })
 
 var clickHandler = function(e){
- 	$('#info').empty();
-  
-  	var feature = e.target.feature;
-  
-  	$('#sidebar').fadeIn(400, function(){
-      var info = '';
-      
-      info += '<div>';
-      info += '<h2>' + feature.properties.name + '</h2>'
-      if(feature.properties.cuisine) info += '<p>' + feature.properties.cuisine + '</p>';
-      if (feature.properties.phone)  info += '<p>' + feature.properties.phone + '</p>';
-      if(feature.properties.phone) info +=   '<p><a href="' + feature.properties.website + '">'  + feature.properties.website + '</a></p>'
-      info += '</div>';
-      $('#info').append(info);
-    })
-}
+  $('#info').empty();
+
+  var feature = e.target.feature;
+
+  $('#info').fadeIn(400,function(){
+    var info = '';
+
+    info += '<div>'
+    info += '<h2>' + feature.properties.name + '</h2>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.cuisine + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.phone + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.website + '</p>'
+    if(feature.properties.phone) info +=   '<p><a href="' + feature.properties.website + '">'  + feature.properties.website + '</a></p>'
+    info += '</div>'
+
+    $('#info').append(info);
+  });
+};
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.on('click',clickHandler);
-  })
-})
+    layer.on('click', clickHandler);
+  });
+});
 
 map.on('click',function(e){
-	$('#info').fadeOut(200);
-  $('#info').empty();
+    $('#info').fadeOut(200);
+    $('#info').empty();
 });
